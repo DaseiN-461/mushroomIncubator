4eb908884a72c4a89da333e6a93764167ed6cfdd
Franco <frank_kings@outlook.com>
2023-08-14 21:01:07 +0200

    Se comienza otra rama para el desarrollo del PID.

---
+++ pid_freertos_30jul_00_12/defs.h
@@ -0,0 +1,238 @@
+////////////////////////////////////////////////// PID Parametros ////////////////////////////////////////////////// 
+
+struct struct_configFromEEPROM {
+  double setpoint;
+  bool enable; 
+};
+
+ typedef struct {
+  String id;
+  bool enable;
+  double setpoint, input, output;
+  double kp, ki, kd;
+  int windowSize;
+  unsigned long windowStartTime;
+  unsigned long windowCurrentTime;
+  double threshold;
+  double output_limit_max;
+  double output_limit_min;
+} PIDTaskArguments;
+
+
+PIDTaskArguments pidTempArgs = {
+    .id="Temperature",
+    .enable=true,
+    .setpoint=27,
+    .kp=200, .ki=1, .kd=1,
+    .windowSize = 2000,
+    .threshold = 2,
+    .output_limit_max = 1/2,
+    .output_limit_min = 1
+  };
+
+PIDTaskArguments pidHumArgs = {
+    .id="Humidity",
+    .enable=false,
+    .setpoint=90,
+    .kp=500, .ki=1, .kd=1,
+    .windowSize=5000,
+    .threshold = 5,
+    .output_limit_max = 1/2,
+    .output_limit_min = 1
+  };
+
+void save_config(){
+        // Crear una instancia de la estructura
+        struct_configFromEEPROM temp;
+        struct_configFromEEPROM hum;
+      
+        // Asignar valores a la estructura
+        temp.setpoint = pidTempArgs.setpoint;
+        temp.enable = pidTempArgs.enable;
+
+        hum.setpoint = pidHumArgs.setpoint;
+        hum.enable = pidHumArgs.enable;
+      
+        // Guardar la estructura en la dirección 0 de la EEPROM
+        EEPROM.put(0, temp);
+        delay(1000);
+        EEPROM.put(15, hum);
+        delay(1000);
+      
+        EEPROM.commit();
+        // Finalizar la EEPROM
+        EEPROM.end();
+}
+
+void load_config(){
+        struct_configFromEEPROM temp;
+        struct_configFromEEPROM hum;
+
+        EEPROM.get(0, temp);
+        EEPROM.get(15, hum);
+
+        // Acceder a los valores individuales
+        double temp_setpoint = temp.setpoint;
+        bool temp_enable = temp.enable;
+      
+        double hum_setpoint = hum.setpoint;
+        bool hum_enable = hum.enable;
+
+        // Cargar a la configuracion
+        pidTempArgs.enable = temp_enable;
+        pidTempArgs.setpoint = temp.setpoint;
+
+        pidHumArgs.enable = hum_enable;
+        pidHumArgs.setpoint = hum.setpoint;
+}
+
+//////////////////////////////////////// PID /////////////// GPIOs de entradas y salidas
+
+// Relé de salida al actuador de Temperatura
+        // PIN de salida
+#define RELAY_PIN_HEATER 4
+        // Indicador de salida (LED_BUILTIN ESP32 DEVKIT V1)
+#define LED_PIN 2
+
+// Relé de salida al actuador de Humedad
+#define RELAY_PIN_HUMIDIFIER 12
+
+  
+////////////////////////////////////////////////// FreeRTOS //////////////////////////////////////////////////
+TaskHandle_t pidTempTaskHandle, pidHumTaskHandle;
+TaskHandle_t serialTaskHandle, oledTaskHandle, fsmTaskHandle; 
+
+
+
+
+///////////////////////////////////////////////// DHT22 SENSOR ////////////////////////////////////////////////////
+
+#define DHTPIN 23
+#define DHTTYPE DHT21
+
+DHT dht(DHTPIN, DHTTYPE);
+
+
+////////////////////////////////////////////////// USER INTERFACE BUTTONS //////////////////////////////////////////////////
+
+#define TOUCH_PIN_UP  T9  // Pin táctil T4 pin13
+#define TOUCH_PIN_DOWN  T4  // Pin táctil T9 pin32
+#define TOUCH_PIN_SEL  T7  // Pin táctil T7 pin27
+#define TOUCH_PIN_LEFT  T0  // Pin táctil T0 pin04
+#define TOUCH_PIN_RIGHT  T3  // Pin táctil T3 pin15
+
+volatile bool touchUpPressed = false;
+volatile bool touchDownPressed = false;
+volatile bool touchSelPressed = false;
+volatile bool touchLeftPressed = false;
+volatile bool touchRightPressed = false;
+
+
+ 
+
+ /////////////////////////////////////////////// FSM estados //////////////////////////////////////////////////////
+ enum state_UI{
+  
+  pid_monitor,
+
+  sel_config,
+ 
+  sel_pid,
+  sel_pid_enable,
+  sel_pid_setpoint,
+  sel_pid_windowSize
+  
+};
+
+bool pid_temp = true; // pid temp defaul, false = hum
+bool config_pid = true;
+bool pid_enable = true;
+
+
+
+/////////////////////////////////////////// I2C /////////////////////////////////////////////////
+
+
+#define SDA_PIN 21
+#define SCL_PIN 22
+
+
+///////////////////////////////////////////////// OLED ////////////////////////////////////////////////////
+
+#define OLED_ADDRESS 0X3C
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////// FUNCTIONS ///////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////// SALIDAS DEL PID ////////////////////////////
+// Salida del pin de PID a HIGH
+void outputPIDPin_HIGH(String pidID){
+        if(pidID == "Temperature"){
+                digitalWrite(RELAY_PIN_HEATER, HIGH);
+                digitalWrite(LED_PIN, HIGH);
+        }else if(pidID == "Humidity"){
+                // do output pid humidity HIGH
+                digitalWrite(RELAY_PIN_HUMIDIFIER, HIGH);
+        }
+}
+
+// Salida del pin de PID a LOW
+void outputPIDPin_LOW(String pidID){
+        if(pidID == "Temperature"){
+                digitalWrite(RELAY_PIN_HEATER, LOW);
+                digitalWrite(LED_PIN, LOW);
+        }else if(pidID == "Humidity"){
+                // do output pid humidity LOW
+                digitalWrite(RELAY_PIN_HUMIDIFIER, LOW);
+        }
+}
+
+
+// 
+void pidToActuator(void* pvParameters){
+        PIDTaskArguments* args = (PIDTaskArguments*)pvParameters;
+        
+        // Si el tiempo transcurrido desde el inicio de la nueva ventana es mayor que el tamaño de la ventana
+        // Entonces reinicia la ventana actualizando el valor de inicio de la nueva ventana
+        if (millis() - args->windowStartTime > args->windowSize){ 
+                args->windowStartTime += args->windowSize;
+        }
+
+        // Actualiza el valor del tiempo transcurrido desde el inicio de la nueva ventana
+        args->windowCurrentTime = millis() - args->windowStartTime;
+
+        // Si al salida del control PID es mayor que el tiempo transcurrido desde el inicio de la nueva ventana
+        // Entonces activa el pin de salida del relay, sino la desactiva
+        if (args->output > millis() - args->windowStartTime){
+                outputPIDPin_HIGH(args->id);
+        }else{
+                outputPIDPin_LOW(args->id);
+        }
+}
+
+
+/////////////////////////////////////// TOUCH BUTTONS INTERRUPTIONS ////////////////////////////////
+
+void IRAM_ATTR touchUpISR() {
+  touchUpPressed = true;
+}
+
+void IRAM_ATTR touchDownISR() {
+
+  touchDownPressed = true;
+}
+
+void IRAM_ATTR touchSelISR() {
+  touchSelPressed = true;
+}
+void IRAM_ATTR touchLeftISR() {
+  touchLeftPressed = true;
+}
+void IRAM_ATTR touchRightISR() {
+  touchRightPressed = true;
+ }
---
+++ pid_freertos_30jul_00_12/pid_freertos_30jul_00_12.ino
@@ -0,0 +1,288 @@
+#include "libs.h"
+#include "defs.h"
+#include "userInterfaceFSM.h"
+
+
+
+
+void setup()
+{
+  
+        Serial.begin(115200);
+        Wire.begin(SDA_PIN, SCL_PIN);
+        dht.begin();
+
+        // La memoria EEPROM del ESP32 es de 4096 BYTES
+        EEPROM.begin(4096);
+
+        // Cargar la configuracion guardada en la EEPROM
+        load_config();
+      
+      
+      
+        pinMode(LED_PIN, OUTPUT);
+        pinMode(DHTPIN, INPUT);
+       
+      
+        // Configurar los pines táctiles como entradas
+        touchAttachInterrupt(TOUCH_PIN_UP, touchUpISR, 20); // Umbral de sensibilidad táctil (ajustar según necesidades)
+        touchAttachInterrupt(TOUCH_PIN_DOWN, touchDownISR, 20); // Umbral de sensibilidad táctil (ajustar según necesidades)
+        touchAttachInterrupt(TOUCH_PIN_SEL, touchSelISR, 20); // Umbral de sensibilidad táctil (ajustar según necesidades)
+        touchAttachInterrupt(TOUCH_PIN_LEFT, touchLeftISR, 20); // Umbral de sensibilidad táctil (ajustar según necesidades)
+        touchAttachInterrupt(TOUCH_PIN_RIGHT, touchRightISR, 20); // Umbral de sensibilidad táctil (ajustar según necesidades)
+      
+        
+        oledSetup();
+        
+      
+        
+        //xTaskCreatePinnedToCore(task_PID, "PID hum", 20*1024, &pidHumArgs, 3, &pidHumTaskHandle,0);
+        
+        //xTaskCreatePinnedToCore(task_serialPrint, "serial print temp", 1*1024, &pidTempArgs, 1, &serialTaskHandle,1);
+        //xTaskCreatePinnedToCore(task_serialPrint, "serial print hum", 1*1024, &pidHumArgs, 1, &serialTaskHandle,1);
+      
+      
+        xTaskCreatePinnedToCore(task_MONITOR, "[oled] monitor task", 10*1024, NULL, 1, &oledTaskHandle,1);
+        xTaskCreatePinnedToCore(task_FSM, "[oled] user interface fsm", 20*1024, &pidTempArgs, 5, &fsmTaskHandle,1);
+        
+        xTaskCreatePinnedToCore(task_PID_temperature, "PID temp", 20*1024, NULL, 3, &pidTempTaskHandle,0);
+
+        xTaskCreatePinnedToCore(mainTask, "main Task", 10*1024, NULL, 2, NULL,1);
+        
+      
+       
+      
+        //vTaskSuspend(pidTempTaskHandle);
+        //vTaskSuspend(pidHumTaskHandle);
+        //vTaskSuspend(serialTaskHandle);
+        //vTaskSuspend(oledTaskHandle);
+  
+}
+
+void loop()
+{
+  
+
+}
+
+void task_PID_temperature(void* pvParameters){
+  
+        PID pid_temperature(&pidTempArgs.input,
+                            &pidTempArgs.output,
+                            &pidTempArgs.setpoint,
+                            pidTempArgs.kp,
+                            pidTempArgs.ki,
+                            pidTempArgs.kd,
+                            DIRECT);
+        //turn the PID on
+        pid_temperature.SetMode(AUTOMATIC);
+        
+        while(1){
+
+
+                // Si el error es menor al umbral, disminuir el máximo de la salida
+                if(pidTempArgs.setpoint-pidTempArgs.input<pidTempArgs.threshold){
+                        pid_temperature.SetOutputLimits(0, pidTempArgs.windowSize*pidTempArgs.output_limit_min);
+                }else{
+                        pid_temperature.SetOutputLimits(0, pidTempArgs.windowSize*pidTempArgs.output_limit_max);
+                }
+
+
+                // Actualiza el valor de salida del PID
+                pid_temperature.Compute();
+                pidToActuator(&pidTempArgs);
+
+                vTaskDelay(100/portTICK_PERIOD_MS);
+        }
+}
+
+void task_PID(void* pvParameters){
+        PIDTaskArguments* args = (PIDTaskArguments*)pvParameters;
+        PID pid(&args->input, &args->output, &args->setpoint, args->kp, args->ki, args->kd, DIRECT);
+        
+        //tell the PID to range between 0 and the full window size
+        pid.SetOutputLimits(0, args->windowSize);
+        
+      
+        
+      
+        args->windowStartTime = millis();
+        bool measureOK = false;
+        while(1){
+          
+                // Mientras no se obtenga una lectura del sensor adecuada, el sistema no continúa y se queda detenida en el búcle
+                while(!measureOK){
+                        if(args->id=="Temperature"){
+                                args->input = dht.readTemperature();
+                        }else if(args->id=="Humidity"){
+                                args->input = dht.readHumidity();
+                        }
+                        if (isnan(args->input)) {
+                                Serial.println(F("Failed to read from DHT sensor!"));
+                                // Advertencia en pantalla OLED sobre la falla en el sensor.
+                        }else{
+                                measureOK = true;
+                        }
+                }
+
+                // Actualiza el valor de salida del PID
+                pid.Compute();
+
+                // La salida del PID la utiliza para modular las conmutaciones del rele de salida
+                pidToActuator(pvParameters);
+            
+                measureOK = false;
+                vTaskDelay(50/portTICK_PERIOD_MS);
+        }
+}
+
+
+
+
+
+
+
+void task_MONITOR(void* pvParameters){
+  
+        while(1){
+                pidTempArgs.input = dht.readTemperature();
+                pidHumArgs.input = dht.readHumidity();
+
+                state_monitor_printOled();
+                
+                vTaskDelay(500/portTICK_PERIOD_MS);
+        }
+}
+
+
+
+
+
+// Tarea para administrar las tareas: Monitor y máquina de estados
+void mainTask(void* pvParameters){
+
+        while(1){
+                //////////////////////////////////////////////////////////////////////////////
+                ///////////////////////////// MONITOR TASK //////////////////////////////////////////////
+                //////////////////////////////////////////////////////////////////////////////
+                // Condicion para administrar la tarea del monitor: cuando no se está utilizando.
+                if(currentStateUI == pid_monitor){
+                        // Si el estado actual de la máquina es el monitor y la tarea se encuentra suspendida,
+                        // entonces reanuda la tarea del monitor.
+                        if(eTaskGetState(oledTaskHandle)==eSuspended){
+                                vTaskResume(oledTaskHandle);
+                                Serial.println("hello from pid_monitor");
+                        }
+                }else{
+                        // Si el estado actual no es el monitor y la tarea no se encuentra suspendida,
+                        // entonces suspende la tarea del monitor.
+                        if(eTaskGetState(oledTaskHandle)!= eSuspended){
+                                Serial.println("fsmTaskSuspended");
+                                vTaskSuspend(oledTaskHandle);
+                        }      
+                }
+
+                //////////////////////////////////////////////////////////////////////////////
+                ///////////////////////////// USER INTERFACE FSM TASK //////////////////////////////////////////////
+                //////////////////////////////////////////////////////////////////////////////
+                // Condicion para administrar la tarea de la máquina de estados: si ningún botón es presionado.
+                if(touchUpPressed or touchDownPressed or touchLeftPressed or touchRightPressed or touchSelPressed){
+
+                        // Si al menos un botón está siendo presionado.
+                        //Si la tarea estaba suspendida, se reanuda.
+                        if(eTaskGetState(fsmTaskHandle)==eSuspended){
+                                vTaskResume(fsmTaskHandle);
+                          
+                        }
+                }else{
+                        // Si ningún botón es presionado.
+                        // Si la tarea no estaba suspendida, se suspende.
+                        if(eTaskGetState(fsmTaskHandle)!= eSuspended){
+                                vTaskSuspend(fsmTaskHandle);
+                        }
+                }
+
+                //////////////////////////////////////////////////////////////////////////////
+                ///////////////////////////// MONITOR TASK //////////////////////////////////////////////
+                //////////////////////////////////////////////////////////////////////////////
+                // Condición para administrar la tarea de PID:
+
+                ///////////////////////// PID TEMPERATURA ////////////////////////////////////
+                 
+                // Si el valor de enable está activo.
+                if(pidTempArgs.enable){
+                        // Si la tarea estaba suspendida
+                        if(eTaskGetState(pidTempTaskHandle)==eSuspended){
+                                // La reanuda
+                                Serial.println("pidtemp reanudado");
+                                vTaskResume(pidTempTaskHandle);
+                          
+                        }
+                // Si esta inactivo
+                }else{
+                        // Si no estaba suspendida
+                        if(eTaskGetState(pidTempTaskHandle)!= eSuspended){
+                                // La suspende
+                                Serial.println("pidtemp suspendido");
+                                vTaskSuspend(pidTempTaskHandle);
+                        }
+                }
+
+                ///////////////////////// PID HUMEDAD ////////////////////////////////////
+                
+                if(pidHumArgs.enable){
+                        // Si la tarea estaba suspendida
+                        if(eTaskGetState(pidHumTaskHandle)==eSuspended){
+                                // La reanuda
+                                vTaskResume(pidHumTaskHandle);
+                          
+                        }
+                // Si esta inactivo
+                }else{
+                        // Si no estaba suspendida
+                        if(eTaskGetState(pidHumTaskHandle)!= eSuspended){
+                                // La suspende
+                                vTaskSuspend(pidHumTaskHandle);
+                        }
+                }
+
+
+                // Condición para administrar la tarea de TX serial: 
+                vTaskDelay(100/portTICK_PERIOD_MS);
+        }
+}
+
+void task_FSM(void* pvParameters){
+        PIDTaskArguments* args = (PIDTaskArguments*)pvParameters;
+        while(1){
+     
+                stateMachine(currentStateUI, args);
+  
+                switch(currentStateUI){
+                  
+                        case sel_config:
+                                state_selConfig_printOled();
+                                break;
+                          
+                        case sel_pid:
+                                state_selPID_printOled();
+                                break;
+                        case sel_pid_enable:
+                                state_pidEnable_printOled();
+                                break;
+                        
+                 }      
+                 vTaskDelay(50/portTICK_PERIOD_MS);        
+        } 
+}
+
+void task_serialPrint(void* pvParameters){
+        PIDTaskArguments* args = (PIDTaskArguments*)pvParameters;
+        while(1){
+                Serial.printf("\n----------- PID %s", args->id);
+                Serial.printf("\t input: %f, output %f\n",args->input, args->output);
+          
+                //16ms for 60<frames/second
+                vTaskDelay(500/portTICK_PERIOD_MS);
+        }
+}
